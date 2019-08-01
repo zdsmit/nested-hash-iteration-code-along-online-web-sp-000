ef02771e46642e8180e0587c4ac13296607b5973
Zach  Smith <zdsmith001@gmail.com>
2019-08-01 15:09:41 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,9 @@ require 'pry'
   }
 
 def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    binding.pry
+  if person == "Freddie Mercury"
+    contacts.each do |person, contact_details_hash|
+      binding.pry
+    end
   end
 end
